ae8c1d629cddcebefaa378acb5df9498690ffe5b
Andrew Kwolek <andrewkwolek2025@u.northwestern.edu>
2024-12-07 13:34:20 -0600

    Commited first test

--- tests/test_a_setup.py
+++ tests/test_a_setup.py
@@ -15,7 +15,7 @@ def test_setup():
     assert len(lines) == 1, "Just a single line with your NetID"
 
     netid = str(lines[0].strip())
-    assert netid != "NETID_GOES_HERE", "Add your NetID"
+    assert netid == "niv4648", "Add your NetID"
     assert netid.lower() == netid, "Lowercase NetID, please"
-    assert re.search(r"^[a-z]{3}[0-9]{3,4}$", netid) is not None, "Your NetID looks like xyz0123"
-
+    assert re.search(r"^[a-z]{3}[0-9]{3,4}$",
+                     netid) is not None, "Your NetID looks like xyz0123"
